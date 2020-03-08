01ad7c529bed8209935715a54964d1f94776ecda
mathdevelop <web.matheusalves@gmail.com>
2020-03-08 10:07:47 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Instamath
+
+Tenha suas imagens do Instagram no seu site, de maneira simples e rápida.
+
+Todos os Plugins que utilizavam a API do Instagram sofriam com a troca constante de Tokens para manter seu funcionamento, mas agora com o desativação da API básica ao final de março/2020, vão parar de funcionar completamente. A saída encontrada para alguns sites foi adicionar Widgets de terceiros que varem a página do perfil à procura das imagens sem a necessidade da API, estes possuem muitas vezes recursos pagos para funcionar em sua forma completa.
+
+O Instamath foi desenvolvido com o intuito de ser uma solução gratuita para obter diretamente as fotos sem ter nenhum servidor intermediário, oferecendo liberdade para customização e velocidade na execução.
+
+
+## Uso
+
+Instamath não depende de nenhuma biblioteca para o seu funcionamento.
+
+```
+<div id="instamath"></div>
+
+<script>
+    document.querySelector('#instamath').instamath({
+        user: 'mathdevelop', // Obrigatório¹
+        limit: 5, // Máximo: 12
+        resolution: 150, // Resoluções²: 150, 240, 320, 480, 640
+        after: function() {
+            // Função executada após recuperar as imagens da conta (Ideal para Plugins de Carousel)
+        }
+    });
+</script>
+```
+Observações:
+1) O usuário a ser utilizado deve ser público.
+2) Utilizar uma resolução maior e limitar o tamanho por CSS oferecerá uma qualidade maior, pondere o ganho de qualidade com o peso da imagem.
+
+## CDN
+
+Caso você desejar, poderá utilizar o Link abaixo com a versão mais atual ou baixar o arquivo para importar no projeto.
+
+```bash
+https://cdn.jsdelivr.net/gh/mathdevelop/instamath/instamath.min.js
+```
+
+## Demo
+[Instamath com Slick](https://instamath.netlify.com/)
---
+++ instamath.js
@@ -0,0 +1,56 @@
+Object.defineProperty(Object.prototype, 'instamath', {
+    value: function(options) {
+        if(options.user) {
+            var element = this;
+            var http = new XMLHttpRequest();
+
+            http.onreadystatechange = function() {
+                if(this.readyState === 4 && this.status === 200) {
+                    var sharedData = JSON.parse(http.responseText.match(/window._sharedData = (.*);/)[1]);
+                    var limit = options.limit ? options.limit : 12;
+                    var resolution = 0;
+                    var html = '';
+                    
+                    if(!(limit >= 1 && limit <= 12))
+                        limit = 12;
+
+                    switch(options.resolution) {
+                        case 150:
+                            resolution = 0;
+                            break;
+                        case 240:
+                            resolution = 1;
+                            break;
+                        case 320:
+                            resolution = 2;
+                            break;
+                        case 480:
+                            resolution = 3;
+                            break;
+                        case 640:
+                            resolution = 4;
+                            break;
+                    }
+
+                    var itens = sharedData.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media;
+
+                    for(var index = 0; index < limit && index < itens.count; index++) {
+                        var item = itens.edges[index].node;
+
+                        html += '<a href="https://www.instagram.com/p/' + item.shortcode + '/"><img src="' + item.thumbnail_resources[resolution].src + '"></a>';
+                    }
+
+                    element.innerHTML = html;
+
+                    if(options.after)
+                        options.after();
+                }
+            };
+
+            http.open('GET', 'https://www.instagram.com/' + options.user + '/', true);
+
+            http.send();
+        }
+    },
+    enumerable: false
+});

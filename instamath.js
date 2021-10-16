190218ca4af6951cdb165ba9e0b3152fef18897c
Matheus Alves <web.matheusalves@gmail.com>
2021-10-16 17:19:34 -0300

    Adding CORS Proxy

--- instamath.js
+++ instamath.js
@@ -1,3 +1,23 @@
+function instamath_load(image) {
+    var http = new XMLHttpRequest();
+
+    http.onreadystatechange = function() {
+        if(this.readyState === 4 && this.status === 200) {
+            var codeUnits = new Uint8Array(http.response);
+            var binary = String.fromCharCode.apply(null, codeUnits);
+            var base64 = btoa(binary);
+
+            image.src = 'data:image/jpeg;base64,' + base64;
+        }
+    };
+
+    http.open('GET', 'https://try.readme.io/' + image.dataset.src, true);
+
+    http.responseType = 'arraybuffer';
+
+    http.send();
+}
+
 Object.defineProperty(Object.prototype, 'instamath', {
     value: function(options) {
         if(options.user) {
@@ -30,6 +50,8 @@ Object.defineProperty(Object.prototype, 'instamath', {
                         case 640:
                             resolution = 4;
                             break;
+                        default:
+                            options.resolution = 150;
                     }
 
                     var itens = sharedData.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media;
@@ -41,7 +63,7 @@ Object.defineProperty(Object.prototype, 'instamath', {
                             if(limit < 12)
                                 limit += 1;
                         } else {
-                            html += '<a href="https://www.instagram.com/p/' + item.shortcode + '/"><img src="' + item.thumbnail_resources[resolution].src + '"></a>';
+                            html += '<a href="https://www.instagram.com/p/' + item.shortcode + '/"><img src="https://via.placeholder.com/' + options.resolution + '/FFFFFF/FFFFFF" data-src="' + item.thumbnail_resources[resolution].src + '" onload="instamath_load(this)"></a>';
                         }
                     }
 
@@ -56,7 +78,7 @@ Object.defineProperty(Object.prototype, 'instamath', {
                 }
             };
 
-            http.open('GET', 'https://www.instagram.com/' + options.user + '/', true);
+            http.open('GET', 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=https://www.instagram.com/' + options.user + '/', true);
 
             http.send();
         }
